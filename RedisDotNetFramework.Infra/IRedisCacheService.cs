3bcf9867a4fcf97e9eae136ca95403d74e56822d
Behzad Parsa <behzad.parsa1996@yahoo.com>
2023-09-18 09:05:19 +0330

    Redis interface was added

---
+++ RedisDotNetFramework.Infra/IRedisCacheService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisDotNetFramework.Infra
+{
+    public interface IRedisCacheService
+    {
+        T GetObject<T>(string key, int dbNumber);
+        bool SetObject<T>(string key, T value, int dbNumber, TimeSpan? expireTime = null);
+        bool RemoveKey(string key, int dbNumber);
+
+        bool RemoveDatabase(int dbNumber);
+        bool RemoveDatabase();
+        List<T> GetList<T>(string key, int dbNumber);
+        bool InsertToEndOfList<T>(string key, T value, int dbNumber);
+        bool SetList<T>(string key, List<T> value, int dbNumber);
+    }
+}
---
+++ RedisDotNetFramework.Infra/IRedisStore.cs
@@ -0,0 +1,15 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisDotNetFramework.Infra
+{
+    public interface IRedisStore
+    {
+        IDatabase GetRedisDatabase(int dbNumber);
+        IServer RedisServer();
+    }
+}
